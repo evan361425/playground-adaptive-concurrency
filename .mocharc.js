40e68f5db6f54f38d896988233d4b344c176d549
Shueh Chou Lu <evanlu361425@gmail.com>
2022-04-19 11:57:07 +0800

    Initial commit

---
+++ .mocharc.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+  require: ['ts-node/register', 'source-map-support/register'],
+  recursive: true,
+  spec: 'test/**/*.spec.ts',
+};
---
+++ test/index.spec.ts
@@ -0,0 +1,7 @@
+import { expect } from 'chai';
+
+describe('Some Class', function () {
+  it('should pass', function () {
+    expect(true).to.eq(true);
+  });
+});
